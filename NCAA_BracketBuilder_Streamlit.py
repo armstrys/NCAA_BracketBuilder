7ff455e600cb7392178f32ca8979113906717f11
Ryan Armstrong <armstrys@Ryans-MacBook-Pro.local>
2021-03-08 14:15:08 -0600

    updates for 2021

--- NCAA_BracketBuilder_Streamlit.py
+++ NCAA_BracketBuilder_Streamlit.py
@@ -5,26 +5,24 @@ import graphviz as graphviz #installed with pip
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import glob
 
 path = Path('./input/')  #path for data files
 
-## please put the name of your submission file here
-submission_file = path/'submission.csv' 
-
 ## you can change the other file names here, or just use the Kaggle names and run the dashboard as is.
-mw = (st.radio(label='Men\'s or Women\'s submission?',options=['Men','Women'] ))[0][0]
+submission_file = st.sidebar.file_uploader(label='Drag your Kaggle solution file here')
+# submission_file = st.sidebar.selectbox(label='Choose submission file from list',options=glob.glob('./input/*submission.csv'))
+mw = (st.sidebar.radio(label='Men\'s or Women\'s submission?',options=['Men','Women'] ))[0][0]
 
 seasons_file = path/(mw+'Seasons.csv')
 teams_file = path/(mw+'Teams.csv')
 seeds_file = path/(mw+'NCAATourneySeeds.csv')
 slots_file = path/(mw+'NCAATourneySlots.csv')
 
-
 #Info
 '''
 ## NCAA Bracket Builder
-Please place your submission file (named: 'submission.csv') and other data in the input folder with the names described in
-'./input/info.txt' or you can go into the code and edit the input file paths at the top of the file.
+Please look at the settings in the side bar to get started. You will add your solution file there.
 
 This GUI will walk you through the bracket game by game. Periodically a table will show you your picks (that you can
 eventually export) so that you can make sure things are on track. The slider at the top lets you choose a threshold for
@@ -35,12 +33,13 @@ check the table before you export your results).
 
 At the end you will have the option to export your final picks and you will be able to see a quick summary of your bracket
 that you can screenshot for reference - the bracket is also a good way to check that your selections are okay. Unfortunately,
-exporting the bracket requires a standalone installation of Graphvis and isn't an option at this time.
+exporting the bracket requires a standalone installation of Graphviz and isn't an option at this time (so have fun squinting).
 
 [Streamlit](https://www.streamlit.io/), the tool used to build this allows you to change the settings to 'wide mode' in the
 upper right, which makes the chart more visible. Thanks to Streamlit for building this easy to use tool and all the folks
-at Kaggle for putting on the [Google Cloud & NCAA® ML Competition 2020-NCAAM]
-(https://www.kaggle.com/c/google-cloud-ncaa-march-madness-2020-division-1-mens-tournament)!
+at Kaggle for putting on the March Machine Learning Mania [Men's]
+(https://www.kaggle.com/c/ncaam-march-mania-2021) and [Women's]
+(https://www.kaggle.com/c/ncaaw-march-mania-2021) tournaments!
 '''
 
 ##loading function
@@ -49,7 +48,7 @@ def load_submission(df,slots,seeds,season_info):
     df.reset_index(inplace=True, drop=True)
     df = df[['ID','Season','LeftTeamID','RightTeamID','Pred']]
     df.columns = ['ID','Season','LeftTeamID','RightTeamID','Pred']
-    season = int(df['Season'].unique()[0])
+    season = int(st.sidebar.selectbox(label='Season',options=df['Season'].unique()))
     season_info = season_info.loc[season_info['Season']==season].copy()
     region_dict = {'W':season_info['RegionW'].values[0],
                    'X':season_info['RegionX'].values[0],
@@ -89,9 +88,20 @@ season_info = pd.read_csv(seasons_file)
 teams_dict = pd.read_csv(teams_file).set_index('TeamID')['TeamName'].to_dict() # Create team dictionary to go from team ID to team name
 seeds = pd.read_csv(seeds_file)
 slots = pd.read_csv(slots_file)
-submission = pd.read_csv(submission_file)
+
+try:
+    submission = pd.read_csv(submission_file)
+except: 
+    st.warning('Please add a valid solution file and adjust settings to continue!')
 
 submission, slots, seeds_dict, season = load_submission(submission,slots,seeds,season_info)
+
+stocastic = st.sidebar.radio('Stocastic or Deterministic Bracket?', ['Deterministic','Stochastic']) == 'Stochastic'
+st.sidebar.write('''
+                 A deterministic bracket will always select the team favored by the model. The stocastic
+                 bracket will randomize the winner for each game using the model probabilities. You will get a
+                 different bracket each time you run the model!
+                 ''')
 
 games = slots.copy()
 games['WinnerSeed'] = ''
@@ -101,7 +111,7 @@ games['WinnerName'] = ''
 games['StrongID'] = ''
 games['WeakID'] = ''
 games['WinnerID'] = ''
-games.loc[:,'Pred'] = -1.0
+games.loc[:,'Pred'] = np.nan
 game_cols = games.columns.to_list()
 new_cols = [game_cols[8]]+game_cols[6:8]+game_cols[4:6]+[game_cols[12]]+game_cols[9:12]+game_cols[0:4]
 games = games[new_cols]
@@ -115,16 +125,20 @@ def update_games(games,round,next_round):
         games.loc[idx,'WeakID'] = seeds_dict[row['WeakSeed']]
         games.loc[idx,'StrongName'] = teams_dict[games.loc[idx,'StrongID']]
         games.loc[idx,'WeakName'] = teams_dict[games.loc[idx,'WeakID']]
+        games.sort_values(by=['Round','StrongSeed'],inplace=True)
 
     
     for idx,row in games[games['Round']==round].iterrows():
 
-
+        if stocastic==True:
+            winThresh = np.random.rand()
+        else:
+            winThresh = .5
 
         game = row['Game']
         id = (str(season)+'_'+ str(row['StrongID'])+'_'+ str(row['WeakID']))
         pred = submission.loc[submission['ID']==id,'Pred'].values[0]
-        if pred> .5:
+        if pred> winThresh:
             winslot = row['StrongSeed']
             winID = row['StrongID']
             winname = teams_dict[winID]
@@ -141,15 +155,15 @@ def update_games(games,round,next_round):
             pred = 1 - pred
 
         st.subheader( row['StrongSeed'] +' **' + row['StrongName'] + '** vs ' + 
-                 row['WeakSeed'] + ' **' + row['WeakName'] + '**')
-        if pred < thresh:
-            overwrite = st.radio(label='Manual pick:',options=[winname,losename])
-            if overwrite == losename:
-                winslot = loseslot
-                winID = loseID
-                winname = losename
-                pred = 1 - pred
-            else: pass
+                row['WeakSeed'] + ' **' + row['WeakName'] + '**')
+        
+        overwrite = st.radio(label='Manual pick:',options=[winname,losename])
+        if overwrite == losename:
+            winslot = loseslot
+            winID = loseID
+            winname = losename
+            pred = 1 - pred
+        
 
         st.write( str(winname) + ' predicted to win with a ' + str(np.round(pred*1000)/10) + '% chance')
         st.write('**' + winname + '** advances!')
@@ -181,7 +195,6 @@ def update_games(games,round,next_round):
 
     return games
 
-thresh = st.slider(label='Threshold for manual picking (use 1 to pick all games - less for close games)',min_value=.5,max_value=1.0,value=.6)
 
 if mw == 'M': # no play-in for the womens tourney
     st.header('Play-in games')
@@ -215,31 +228,33 @@ if st.button('Export Picks to .csv'):
 st.header('Okay... where\'s my final data? Check your bracket below! Keep scrolling...')
 bracket_odds = int(round(1/np.multiply.reduce(np.array(games['Pred']))))
 bracket_odds_noPI = int(round(1/np.multiply.reduce(np.array(games.loc[games['Round']!= 'R0','Pred']))))
+games['logloss'] = -np.log(games['Pred'])
+logloss = np.mean(games.loc[games['Round']!= 'R0','logloss'])
 
 if mw=='W':
     st.write('''
             According to these probabilities, your odds of a perfect bracket are 1 in **{a:,d}**...  
-            Yikes! Good luck! :)
-            '''.format(a=bracket_odds))
+            Yikes! Good luck! :) \n\n The expected logloss of this bracket outcome is {logloss}.
+            '''.format(a=bracket_odds,logloss=logloss))
 else:
     st.write('''
             According to these probabilities, your odds of a perfect bracket are 1 in **{a:,d}** including
             the play-in games or **{b:,d}** not including the play-in games...  
-            Yikes! Good luck! :)
-            '''.format(a=bracket_odds,b=bracket_odds_noPI))
+            Yikes! Good luck! :) \n\n The expected logloss of this bracket outcome is {logloss}.
+            '''.format(a=bracket_odds,b=bracket_odds_noPI,logloss=logloss))
 
 ## Quick bracket viz
 graph = graphviz.Digraph(node_attr={'shape': 'rounded','color': 'lightblue2'})
 
 round_dict = {'R0':'R1',
-              'R1':'R2',
-              'R2':'R3',
-              'R3':'R4',
-              'R4':'R5',
-              'R5':'R6',
-              'R6':'CH',
-              'CH':'Winner!'
-              }
+            'R1':'R2',
+            'R2':'R3',
+            'R3':'R4',
+            'R4':'R5',
+            'R5':'R6',
+            'R6':'CH',
+            'CH':'Winner!'
+            }
 for _,row in games.iterrows():
 
     T1 = row['Round']+'-'+row['StrongSeed']+'-'+row['StrongName']
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### NCAA Bracket Builder
 ##### Tweak your picks to create real brackets to beat your friends
 
-This is a quick dashboard I threw together to interact with my predictions from the [Google Cloud & NCAA® ML Competition 2020-NCAAM](https://www.kaggle.com/c/google-cloud-ncaa-march-madness-2020-division-1-mens-tournament) competition on Kaggle. You will need to drop some of the Kaggle formatted data and your submission file into the inputs folder and then run the dashboard using [Streamlit](https://www.streamlit.io/). To start, put the necessary data in the input folder and rename or edit the file paths/names at the top of thes streamlit code. Finally, type:
+This is a quick dashboard I threw together to interact with my predictions from the [March Machine Learning Mania 2021](https://www.kaggle.com/c/ncaam-march-mania-2021) competition on Kaggle. You will need to drop some of the Kaggle formatted data and your submission file into the inputs folder and then run the dashboard using [Streamlit](https://www.streamlit.io/). To start, put the necessary data in the input folder and rename or edit the file paths/names at the top of thes streamlit code. Finally, type:
 
 ```
 streamlit run NCAA_BracketBuilder_Sreamlit.py
@@ -9,6 +9,6 @@ streamlit run NCAA_BracketBuilder_Sreamlit.py
   
 Dependencies:
 * [Streamlit](https://github.com/streamlit/streamlit)
-* [Graphvis](https://pypi.org/project/graphviz/) - (Python implementation only - standalone not needed)
+* [Graphviz](https://pypi.org/project/graphviz/) - (Python implementation only - standalone not needed)
 * [Pandas](https://github.com/pandas-dev)
 * [Numpy](https://github.com/numpy/numpy)
